fcd3195409890f527938141ac8c0622395a89f17
David mumford <37568498+dmumford@users.noreply.github.com>
2021-01-14 11:12:33 +0000

    Create diceRoll.py
    initial commit

---
+++ easy/diceRoll.py
@@ -0,0 +1,11 @@
+# Method that emulates a dice roll by generating
+# a random number between 1 and 6
+
+import random
+
+# define function
+def diceRoll():
+    print(random.randint(1, 6))
+    
+# call function
+diceRoll()
